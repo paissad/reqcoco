7ffdfc1449aba9697e287400b311818cd5807f87
paissad <paissad@gmail.com>
2017-05-10 07:47:27 +0200

    Fixed Sonar code smell

--- reqcoco-generator-xlsx/src/main/java/net/paissad/tools/reqcoco/generator/xlsx/parser/XlsxReqSourceParser.java
+++ reqcoco-generator-xlsx/src/main/java/net/paissad/tools/reqcoco/generator/xlsx/parser/XlsxReqSourceParser.java
@@ -2,6 +2,7 @@ package net.paissad.tools.reqcoco.generator.xlsx.parser;
 
 import java.net.URI;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 
 import net.paissad.tools.reqcoco.api.model.Requirement;
@@ -14,7 +15,7 @@ public class XlsxReqSourceParser implements ReqSourceParser {
 	@Override
 	public Collection<Requirement> parse(URI uri, ReqDeclTagConfig declTagConfig, Map<String, Object> options) throws ReqSourceParserException {
 		// TODO Auto-generated method stub
-		return null;
+		return Collections.emptySet();
 	}
 
 }
