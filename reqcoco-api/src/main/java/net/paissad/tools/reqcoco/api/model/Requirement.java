d160231144aee70ce6572899aec36657281119b2
paissad <paissad@gmail.com>
2017-05-10 08:06:36 +0200

    Added equals() & hashcode() methods for Requirement class.

--- reqcoco-api/src/main/java/net/paissad/tools/reqcoco/api/model/Requirement.java
+++ reqcoco-api/src/main/java/net/paissad/tools/reqcoco/api/model/Requirement.java
@@ -97,4 +97,41 @@ public class Requirement implements Serializable {
 		return builder.toString();
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((version == null) ? 0 : version.hashCode());
+		result = prime * result + ((revision == null) ? 0 : revision.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Requirement))
+			return false;
+		Requirement other = (Requirement) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (version == null) {
+			if (other.version != null)
+				return false;
+		} else if (!version.equals(other.version))
+			return false;
+		if (revision == null) {
+			if (other.revision != null)
+				return false;
+		} else if (!revision.equals(other.revision))
+			return false;
+		return true;
+	}
+
 }
