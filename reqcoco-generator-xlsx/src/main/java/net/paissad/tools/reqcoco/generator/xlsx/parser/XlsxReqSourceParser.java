62ddb9e474b4cef1b4f4f36180463a76b81c049d
paissad <paissad@gmail.com>
2017-05-10 07:35:54 +0200

    Init module ReqCoCo Generator Xlsx pour Microsoft Excel .xlsx documents

---
+++ reqcoco-generator-xlsx/src/main/java/net/paissad/tools/reqcoco/generator/xlsx/parser/XlsxReqSourceParser.java
@@ -0,0 +1,20 @@
+package net.paissad.tools.reqcoco.generator.xlsx.parser;
+
+import java.net.URI;
+import java.util.Collection;
+import java.util.Map;
+
+import net.paissad.tools.reqcoco.api.model.Requirement;
+import net.paissad.tools.reqcoco.generator.simple.api.ReqDeclTagConfig;
+import net.paissad.tools.reqcoco.generator.simple.api.ReqSourceParser;
+import net.paissad.tools.reqcoco.generator.simple.exception.ReqSourceParserException;
+
+public class XlsxReqSourceParser implements ReqSourceParser {
+
+	@Override
+	public Collection<Requirement> parse(URI uri, ReqDeclTagConfig declTagConfig, Map<String, Object> options) throws ReqSourceParserException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- reqcoco-runner/src/main/java/net/paissad/tools/reqcoco/runner/ReqSourceType.java
+++ reqcoco-runner/src/main/java/net/paissad/tools/reqcoco/runner/ReqSourceType.java
@@ -5,10 +5,11 @@ import net.paissad.tools.reqcoco.generator.docx.parser.DocxReqSourceParser;
 import net.paissad.tools.reqcoco.generator.redmine.parser.RedmineReqSourceParser;
 import net.paissad.tools.reqcoco.generator.simple.api.ReqSourceParser;
 import net.paissad.tools.reqcoco.generator.simple.impl.parser.FileReqSourceParser;
+import net.paissad.tools.reqcoco.generator.xlsx.parser.XlsxReqSourceParser;
 
 public enum ReqSourceType {
 
-	FILE(new FileReqSourceParser()), DOCX(new DocxReqSourceParser()), REDMINE(new RedmineReqSourceParser());
+	FILE(new FileReqSourceParser()), DOCX(new DocxReqSourceParser()), XLSX(new XlsxReqSourceParser()), REDMINE(new RedmineReqSourceParser());
 
 	@Getter
 	private final ReqSourceParser parser;
