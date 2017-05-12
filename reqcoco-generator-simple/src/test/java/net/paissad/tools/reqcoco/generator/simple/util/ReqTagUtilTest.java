2c759bdcb7a1c9b099338bf21c2a33babbfa5f22
paissad <paissad@gmail.com>
2017-05-12 14:54:52 +0200

    Xlsx parser : implementation ok

--- reqcoco-generator-simple/src/test/java/net/paissad/tools/reqcoco/generator/simple/util/ReqTagUtilTest.java
+++ reqcoco-generator-simple/src/test/java/net/paissad/tools/reqcoco/generator/simple/util/ReqTagUtilTest.java
@@ -1,12 +1,7 @@
 package net.paissad.tools.reqcoco.generator.simple.util;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.Assert;
 import org.junit.Test;
-
-import net.paissad.tools.reqcoco.generator.simple.api.ReqDeclTag;
 
 public class ReqTagUtilTest {
 
@@ -27,33 +22,5 @@ public class ReqTagUtilTest {
 		Assert.assertEquals("i have a tab & space before and after", ReqTagUtil.trimString(" 	i have a tab & space before and after   	"));
 		Assert.assertEquals("I am preprended and appended by a horizontal space",
 		        ReqTagUtil.trimString(" I am preprended and appended by a horizontal space "));
-	}
-
-	@Test
-	public void testBuildReqDeclTagFromMembers() {
-
-		final Map<String, String> tagMembers = new HashMap<>();
-		final ReqDeclTag expectedTag = new ReqDeclTag();
-
-		tagMembers.put("id", "my_id");
-		tagMembers.put("version", "v1.0");
-		tagMembers.put("revision", "r13");
-		tagMembers.put("summary", "my summary");
-
-		expectedTag.setId("my_id");
-		expectedTag.setVersion("v1.0");
-		expectedTag.setRevision("r13");
-		expectedTag.setSummary("my summary");
-
-		Assert.assertEquals(expectedTag, ReqTagUtil.buildReqDeclTagFromMembers(tagMembers));
-
-		tagMembers.put("summary", "diff");
-		Assert.assertNotEquals(expectedTag, ReqTagUtil.buildReqDeclTagFromMembers(tagMembers));
-
-		tagMembers.put("summary", null);
-		Assert.assertNotEquals(expectedTag, ReqTagUtil.buildReqDeclTagFromMembers(tagMembers));
-
-		tagMembers.remove("summary");
-		Assert.assertNotEquals(expectedTag, ReqTagUtil.buildReqDeclTagFromMembers(tagMembers));
 	}
 }
