5c49416b978261f448f1b30dd7a815477ad1f2b1
paissad <paissad@gmail.com>
2017-05-20 17:44:35 +0200

    Remove master source code

--- release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export GPG_TTY=$(tty)
-mvn clean release:clean release:prepare -P release release:perform
--- reqcoco-parser-simple/src/main/java/net/paissad/tools/reqcoco/parser/simple/impl/tag/SimpleReqTagSourceConfig.java
+++
@@ -1,9 +0,0 @@
-package net.paissad.tools.reqcoco.parser.simple.impl.tag;
-
-public class SimpleReqTagSourceConfig extends AbstractReqCodeTagConfig {
-
-	@Override
-	public String getCompleteRegex() {
-		return "(@ReqSourceCode\\s*\\(\\s*.*?\\))";
-	}
-}
