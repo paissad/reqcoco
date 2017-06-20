edb8b6d93874e71bb58c14cbb1b59a71a9b44f08
paissad <paissad@gmail.com>
2017-06-20 09:12:10 +0200

    Update release script in order to propose to update CHANGELOG and to execute release:perform on demand

--- release.sh
+++ release.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-export GPG_TTY=$(tty)
-mvn clean release:clean release:prepare -P release
-# mvn release:perform -P release
+echo "Did you already update the CHANGELOG.md file ? And eventually the README.md file ? (yes/no)"
+read answer
+
+if [[ "$answer" == "yes" ]]; then
+    export GPG_TTY=$(tty)
+    mvn clean release:clean release:prepare -P release
+    echo "Execute 'mvn release:perform' right now ??? (yes/no)"
+    read release_perform
+    if [[ "$release_perform" == "yes" ]]; then
+        mvn release:perform -P release
+    fi
+else
+    echo >&2 "Please, check that the CHANGELOG.md file is up to date !!!"
+fi
+
